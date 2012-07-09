b3ee847a1f8522cadacfc622cedd2e5c588b15b2
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-07-09 14:49:15 +0200

    Initial repository layout for puppet module stdlib

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: stdlib
+#
+# This module manages stdlib
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class stdlib {
+
+
+}
---
+++ spec/classes/stdlib_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'stdlib' do
+  it { should contain_class 'stdlib' }
+end
