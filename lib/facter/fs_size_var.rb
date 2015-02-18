a22fec988fa922573c9cb9408d6e372d9910dbb0
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-02-18 09:43:01 +0100

    Fix retrieval of multiple lines

--- lib/facter/fs_size_var.rb
+++ lib/facter/fs_size_var.rb
@@ -7,7 +7,7 @@ Facter.add(:fs_size_var) do
   setcode do
     case Facter.value(:osfamily)
     when /RedHat/
-      Facter::Util::Resolution.exec("lsblk -o MOUNTPOINT,TYPE,SIZE | grep '/var' | awk '{print $3}'")
+      Facter::Util::Resolution.exec("lsblk -o MOUNTPOINT,TYPE,SIZE | grep '/var' | awk '{print $3}' | uniq")
     end
   end
 end
