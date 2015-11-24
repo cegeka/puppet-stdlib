e7882650351d636bb9ec03c2102f5c63632df105
fdammeke <fabian.dammekens@cegeka.be>
2015-11-24 16:42:59 +0100

    Add stage for filesystem and mount setup

--- manifests/stages.pp
+++ manifests/stages.pp
@@ -1,8 +1,10 @@
 class stdlib::stages {
 
+  stage { 'setup_fs': }
+  stage { 'setup_mount': }
   stage { 'setup_repo': }
   stage { 'setup_packages': }
   stage { 'setup': }
 
-  Stage['setup_repo'] -> Stage['setup_packages'] -> Stage['setup'] -> Stage['main']
+  Stage['setup_fs'] -> Stage['setup_mount'] -> Stage['setup_repo'] -> Stage['setup_packages'] -> Stage['setup'] -> Stage['main']
 }
