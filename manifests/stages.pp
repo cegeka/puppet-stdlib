51fffaa2a286ad6f8cfc8351e7a6ad3d379191fe
Fabian Dammekens <fabian.dammekens@cegeka.be>
2019-02-15 16:11:39 +0100

    [STAGES] Step away from the stages sir!

--- manifests/stages.pp
+++ manifests/stages.pp
@@ -2,12 +2,15 @@ class stdlib::stages {
 
   stage { 'setup_fs': }
   stage { 'setup_mount': }
+  stage { 'setup_directories': }
   stage { 'setup_repo': }
   stage { 'setup_packages': }
   stage { 'setup_users': }
   stage { 'setup': }
 
   Stage['setup_fs'] -> Stage['setup_mount']
-  Stage['setup_users'] -> Stage['setup_repo'] -> Stage['setup_packages'] -> Stage['setup'] -> Stage['main']
+  Stage['setup_users'] -> Stage['setup_directories']
+  Stage['setup_repo'] -> Stage['setup_packages']
+  Stage['setup'] -> Stage['main']
 
 }
