5232911c9993b2e19989e49d3de1201435b688b2
Vandevoort Tim <tim.vandevoort@cegeka.com>
2019-02-15 14:06:57 +0100

    [DBaaS] Changes required to avoid dependency cycle errors when managing the root user password in Puppet

--- manifests/stages.pp
+++ manifests/stages.pp
@@ -4,9 +4,10 @@ class stdlib::stages {
   stage { 'setup_mount': }
   stage { 'setup_repo': }
   stage { 'setup_packages': }
+  stage { 'setup_users': }
   stage { 'setup': }
 
   Stage['setup_fs'] -> Stage['setup_mount']
-  Stage['setup_repo'] -> Stage['setup_packages'] -> Stage['setup'] -> Stage['main']
+  Stage['setup_users'] -> Stage['setup_repo'] -> Stage['setup_packages'] -> Stage['setup'] -> Stage['main']
 
 }
